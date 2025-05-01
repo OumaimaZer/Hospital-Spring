3f4efb88bb789f93e8fd3c10e3ddbd89ad260c7f
OumaimaZer <o.zerhouani@edu.umi.ac.ma>
2025-05-01 23:26:53 +0100

    More Entities

--- src/main/java/net/zerhouani/hospital/entities/RendezVous.java
+++ src/main/java/net/zerhouani/hospital/entities/RendezVous.java
@@ -1,11 +1,26 @@
 package net.zerhouani.hospital.entities;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.util.Date;
 
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class RendezVous {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private Date date;
     private StatusRDV annule;
+    @ManyToOne
     private Patient patient;
+    @ManyToOne
     private Medecin medecin;
+
+    @OneToOne(mappedBy = "rendezVous")
+    private Consultation consultation;
 }
